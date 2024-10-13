7ac7e5e38f49a569eb454ac54e10942f311f83d1
Mauro Vannucci <mauro@vannucci.eu>
2024-10-13 09:33:08 +0200

    Scheduler

---
+++ S_Scheduler.java
@@ -0,0 +1,55 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class S_Scheduler {
+  public static void main(String[] args) {
+    scheduler2();
+  }
+
+  // NON CORRETTO
+  static void scheduler1() {
+    var scheduler = Executors.newScheduledThreadPool(1, Thread.ofVirtual().factory());
+    scheduler.scheduleAtFixedRate(() -> task1(), 0, 1, TimeUnit.SECONDS);
+    scheduler.scheduleAtFixedRate(() -> task2(), 0, 5, TimeUnit.SECONDS);
+    try {
+      Thread.sleep(30000);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  static ScheduledExecutorService scheduler;
+  static ExecutorService donkey;
+
+  static void scheduler2() {
+    scheduler = Executors.newSingleThreadScheduledExecutor();
+    donkey = Executors.newVirtualThreadPerTaskExecutor();
+
+    schedule(() -> task1(), 0, 1, TimeUnit.SECONDS);
+    schedule(() -> task2(), 0, 5, TimeUnit.SECONDS);
+  }
+
+  static void schedule(Runnable command, long delay, long period, TimeUnit unit) {
+    scheduler.scheduleAtFixedRate(() -> donkey.execute(command), delay, period, unit);
+  }
+
+  static void task1() {
+    System.out.println("MULE1");
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {
+    }
+  }
+
+  static void task2() {
+    System.out.println("MULE2 ---");
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {
+    }
+  }
+
+}
